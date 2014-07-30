60b993784ae6c39696a5f12062c41f57a68a9da4
Yuri Schaeffer <yuri@nlnetlabs.nl>
2014-07-30 13:13:13 +0200

    do not reset position on spacebar

--- aircraft.py
+++ aircraft.py
@@ -9,6 +9,7 @@ class Aircraft:
 		self.beamwidth = 0.015 # (m)
 		self.motors = 2
 		self.reset()
+		self.position = array([0, 0, 0])
 
 	def reset(self):
 		self.a_moment = array([0, 0, 0])
@@ -16,7 +17,6 @@ class Aircraft:
 		self.target_force  = [array([0, 0, 0]), array([0, 0, 0])]
 		self.current_force = [array([0, 0, 0]), array([0, 0, 0])]
 		self.normal = array([0, 1, 0])
-		self.position = array([0, 0, 0])
 		self.motor_pos = array([-self.radius, 0, 0]), \
 						 array([+self.radius, 0, 0])
 
